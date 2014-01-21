a4a1ab68a35f80af81e08469447985b76897d92d
zkellerdevelops <zkellerdevelops@gmail.com>
2014-01-20 18:17:30 -0600

    moved flickr json script to its own file, and added some css to make images go full width

---
+++ assets/js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=99895690@N00&lang=en-us&format=json&jsoncallback=?", displayImages);
+
+        function displayImages(data) {
+
+            // Start putting together the HTML string
+            var htmlString = "";
+
+            // Now start cycling through our array of Flickr photo details
+            $.each(data.items, function(i,item){
+
+                // I only want the ickle square thumbnails
+                var sourceSquare = (item.media.m).replace("_m.jpg", "_b.jpg");
+
+                // Here's where we piece together the HTML
+                // htmlString += '<a href="' + item.link + '" target="_blank">';
+                htmlString += '<img class="img-responsive" title="' + item.title + '" src="' + sourceSquare;
+                htmlString += '" alt="'; htmlString += item.title + '" />';
+                htmlString += '</a>';
+                console.log(htmlString);
+
+                if (htmlString) return false;
+
+            });
+
+        // Pop our HTML in the #images DIV
+        $('#images').html(htmlString);
+
+        // Close down the JSON function call
+        }
+});
