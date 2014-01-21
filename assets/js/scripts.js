979aae3f44edf900613c1741236ecdd0087dd135
zkellerdevelops <zkellerdevelops@gmail.com>
2014-01-20 20:34:37 -0600

    styling and content changes. icons added, typograqphy started

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+//+++++ BODY BACKGROUND IMAGE ++++++
 $(document).ready(function () {
 
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=99895690@N00&lang=en-us&format=json&jsoncallback=?", displayImages);
@@ -15,9 +16,7 @@ $(document).ready(function () {
 
                 // Here's where we piece together the HTML
                 // htmlString += '<a href="' + item.link + '" target="_blank">';
-                htmlString += '<img class="img-responsive" title="' + item.title + '" src="' + sourceSquare;
-                htmlString += '" alt="'; htmlString += item.title + '" />';
-                htmlString += '</a>';
+                htmlString += '<body style="background-image: url('+ sourceSquare +')"></body>'
                 console.log(htmlString);
 
                 if (htmlString) return false;
@@ -25,8 +24,44 @@ $(document).ready(function () {
             });
 
         // Pop our HTML in the #images DIV
-        $('#images').html(htmlString);
+        $('head').append(htmlString);
 
         // Close down the JSON function call
         }
 });
+
+
+
+//+++++ DIV BACKGROUND IMAGE ++++++
+// $(document).ready(function () {
+
+//     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=99895690@N00&lang=en-us&format=json&jsoncallback=?", displayImages);
+
+//         function displayImages(data) {
+
+//             // Start putting together the HTML string
+//             var htmlString = "";
+
+//             // Now start cycling through our array of Flickr photo details
+//             $.each(data.items, function(i,item){
+
+//                 // I only want the ickle square thumbnails
+//                 var sourceSquare = (item.media.m).replace("_m.jpg", "_b.jpg");
+
+//                 // Here's where we piece together the HTML
+//                 // htmlString += '<a href="' + item.link + '" target="_blank">';
+//                 htmlString += '<img class="img-responsive" title="' + item.title + '" src="' + sourceSquare;
+//                 htmlString += '" alt="'; htmlString += item.title + '" />';
+//                 htmlString += '</a>';
+//                 console.log(htmlString);
+
+//                 if (htmlString) return false;
+
+//             });
+
+//         // Pop our HTML in the #images DIV
+//         $('#images').html(htmlString);
+
+//         // Close down the JSON function call
+//         }
+// });
